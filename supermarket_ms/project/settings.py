b5408b524564dbdaa24d709f05382adeb4bd17b9
inganibalguerra <inganibalguerra@gmail.com>
2021-07-23 20:21:55 -0500

    feat: se agrega aplicación web para consumir los recursos

--- supermarket_ms/project/settings.py
+++ supermarket_ms/project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'supermarket_ms.apps.SupermarketMSConfig'
+    'supermarket_ms.apps.SupermarketMSConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -126,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
