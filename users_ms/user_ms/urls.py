fba6cb7c20a2c9274420f610ef5e9351b0902ac1
inganibalguerra <inganibalguerra@gmail.com>
2021-07-24 16:26:15 -0500

    la api funciona

--- users_ms/user_ms/urls.py
+++ users_ms/user_ms/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from user_ms.views.user_view import UserList
 from user_ms.views.user_view import UserDetail
+from user_ms.views.user_view import Login
 
 urlpatterns = [
     path('users/', UserList.as_view()),
     path('users/<int:pk>', UserDetail.as_view()),
+    path('login/<str:user>/<str:password>', Login.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- users_ms/user_ms/views/user_view.py
+++ users_ms/user_ms/views/user_view.py
@@ -2,6 +2,7 @@ from user_ms.models.user_model import User
 from user_ms.serializers.user_serializer import UserSerializer
 from rest_framework import mixins
 from rest_framework import generics
+from django.http import JsonResponse
 
 class UserList(mixins.ListModelMixin,
                    mixins.CreateModelMixin,
@@ -32,3 +33,27 @@ class UserDetail(mixins.RetrieveModelMixin,
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+class Login(mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     generics.GenericAPIView):
+                     
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get(self, request, user,password):
+
+        # user = "anibal"
+        # password= "guerra"
+
+        # user = self.request.params.get('user')
+        # password= self.request.params.get('password')
+
+
+        queryset = User.objects.filter(user=(user),password=(password))
+
+        serializer = UserSerializer(instance=queryset, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+        # return JsonResponse(data)
